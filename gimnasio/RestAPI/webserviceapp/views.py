e0acab1f74358f3e41f6067002840f43725ced1b
RubenPallin <113434110+RubenPallin@users.noreply.github.com>
2023-01-30 18:24:51 +0100

    Update views.py

--- gimnasio/RestAPI/webserviceapp/views.py
+++ gimnasio/RestAPI/webserviceapp/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import tClases
 
 def prueba_API(request):
   return HttpResponse("<h1>Hola</h1>");
+
+def obtener_listado_clases(request):
+      lista = tClases.objects.all()
+      respuesta_final = []
+      for fila_sql in lista:
+          diccionario['id'] = fila_sql.id
+          diccionario['nombre'] = fila_sql.nombre
+          diccionario['horarios'] = fila_sql.horarios
+          respuesta_final.append(diccionario)
+         return JsonResponse(respuesta_final, safe=False)
 # Create your views here.
