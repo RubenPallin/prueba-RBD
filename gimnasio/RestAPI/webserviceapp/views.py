8ba5ad1e33f7d88967a02716f1a7bd0496337742
RubenPallin <113434110+RubenPallin@users.noreply.github.com>
2023-01-30 17:49:09 +0100

    Update views.py

--- gimnasio/RestAPI/webserviceapp/views.py
+++ gimnasio/RestAPI/webserviceapp/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def prueba_API (request):
+def prueba_API(request):
   return HttpResponse("<h1>Hola</h1>");
 # Create your views here.
