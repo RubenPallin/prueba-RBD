ab0302b2e4d6a92507a0dcb915bec7062b362f69
Ruben Pallin Gonzalez <rpalling@fpcoruna.afundacion.org>
2023-01-31 13:30:30 +0100

    Añadido

--- gimnasio/RestAPI/webserviceapp/views.py
+++ gimnasio/RestAPI/webserviceapp/views.py
@@ -1,17 +1,23 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from .models import Tclases
 
 def prueba_API(request):
   return HttpResponse("<h1>Hola</h1>");
 
 def obtener_listado_clases(request):
-      lista = tClases.objects.all()
+      lista = Tclases.objects.all()
       respuesta_final = []
       for fila_sql in lista:
+          diccionario = {}
           diccionario['id'] = fila_sql.id
           diccionario['nombre'] = fila_sql.nombre
           diccionario['horarios'] = fila_sql.horarios
           respuesta_final.append(diccionario)
       return JsonResponse(respuesta_final, safe=False)
+
+
+
+
+
 # Create your views here.
